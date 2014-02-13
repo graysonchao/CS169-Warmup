8b78382360a7ea39f482dfb91c9ffb12714a02e8
Grayson Chao <graysonchao@berkeley.edu>
2014-02-13 12:30:25 -0800

    Reorganized unit tests

--- warmup/testAdditional.py
+++ warmup/testAdditional.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import testLib
 
-class TestLoginUser(testLib.RestTestCase):
+class TestLogin(testLib.RestTestCase):
     def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
         """
         Check that the response data dictionary matches the expected values
@@ -17,38 +17,17 @@ class TestLoginUser(testLib.RestTestCase):
         respData = self.makeRequest("/users/login", method="POST", data = { 'user' : 'user1', 'password' : 'password'} )
         self.assertResponse(respData, count = 2)
 
-class TestLoginWrong(testLib.RestTestCase):
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
-
     def testLoginWrong(self):
         respData = self.makeRequest("/users/login", method="POST", data = { 'user' : 'user1', 'password' : 'notthepassword'} )
         self.assertResponse(respData, count = None, errCode =
                 testLib.RestTestCase.ERR_BAD_CREDENTIALS)
-
-class TestLoginTwice(testLib.RestTestCase):
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
-
     def testLoginTwice(self):
         self.makeRequest("/users/add", method="POST", data = { 'user' : 'user1', 'password': 'password' })
         self.makeRequest("/users/login", method="POST", data = { 'user' : 'user1', 'password' : 'password'} )
         respData = self.makeRequest("/users/login", method="POST", data = { 'user' : 'user1', 'password' : 'password'} )
         self.assertResponse(respData, count = 3)
 
-class TestAddTwice(testLib.RestTestCase):
+class TestAdd(testLib.RestTestCase):
     def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
         """
         Check that the response data dictionary matches the expected values
@@ -63,30 +42,10 @@ class TestAddTwice(testLib.RestTestCase):
         respData = self.makeRequest("/users/add", method="POST", data = { 'user' : 'user1', 'password': 'password' })
         self.assertResponse(respData, count = None, errCode = testLib.RestTestCase.ERR_USER_EXISTS)
 
-class TestBadPass(testLib.RestTestCase):
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
-
     def testBadPass(self):
         respData = self.makeRequest("/users/add", method="POST", data = { 'user'
             : 'user1', 'password': '11charstring' * 13 })
         self.assertResponse(respData, count = None, errCode = testLib.RestTestCase.ERR_BAD_PASSWORD)
-
-class TestBadName(testLib.RestTestCase):
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
 
     def testBadName(self):
         respData = self.makeRequest("/users/add", method="POST", data = { 'password' : 'user1', 'user': '11charstring' * 13 })
